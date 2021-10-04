58037722b646fbdd6d1671998d3308a231e2f923
geiapacia <vixiennegeiapacia@gmail.com>
2021-10-04 22:00:20 +0200

    Fixed computerSelection funtion

--- project.js
+++ project.js
@@ -1,11 +1,11 @@
+const playerSelection = prompt("Choose rock, paper, or scissors!");
+const computerSelection = computerPlay();
+
 function computerPlay() {
     let selection = ["rock", "paper", "scissors"],
     selectionUse = selection[Math.floor(Math.random() * selection.length)];
-    alert(selectionUse);
+    return(selectionUse);
 }
-
-const computerSelection = computerPlay();
-const playerSelection = "rock";
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "rock") {
@@ -15,7 +15,7 @@ function playRound(playerSelection, computerSelection){
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats scissors!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats rock!":
+        return "You win! Paper beats rock!";
     } else if (playerSelection === "paper" && computerSelection === "paper") {
         return "It's a draw! Play again!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
@@ -24,10 +24,13 @@ function playRound(playerSelection, computerSelection){
         return "You lose! Rock beats scissors!";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! Scissors beats paper!";
-    } else (playerSelection === "scissors" && computerSelection === "scissors") {
+    } else if(playerSelection === "scissors" && computerSelection === "scissors") {
         return "It's a draw! Play again!";
     }
-}
-
+};
 console.log(playRound(playerSelection, computerSelection));
+
+
+
+
 
